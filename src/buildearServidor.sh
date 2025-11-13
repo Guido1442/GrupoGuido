728a93d12bc8b843e68e1fdda3564c070ed1125d
Guido Cardozo <guidocardozo2004@gmail.com>
2025-11-13 17:33:02 -0300

    Build command render

---
+++ src/buildearServidor.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+cd src
+
+npx tsc
+
+cd ..
+
+mkdir -p dist
+
+cp -f src/crearGenerico.html dist/
+cp -f src/editarGenerico.html dist/
+cp -f src/login.html dist/
+cp -f src/style.css dist/
+cp -f src/styleMenu.css dist/
+
