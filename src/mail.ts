05b4bd508903e15d1505387ebf17ea901bfe1c40
Guido Cardozo <guidocardozo2004@gmail.com>
2025-11-20 12:20:10 -0300

    Mailtrap

--- src/mail.ts
+++ src/mail.ts
@@ -1,22 +1,19 @@
 import nodemailer from 'nodemailer';
 // codigo base: https://nodemailer.com/
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.TOKEN_MAIL,
   },
-  tls: {
-    rejectUnauthorized: false
-  }
 });
 
 
 export async function enviarMail(to: string, certificado: string, titulo: string){
   const mailOptions = {
-        from: '"AIDA API" <aidadb2025@gmail.com>',
+        from: '"AIDA API" <noreply@aida.com>',
         to: to,
         subject: "Certificado de Titulo en Tramite",
         text: `Felicidades!! Conseguiste el titulo de ${titulo}. Te enviamos tu certificado de titulo en Tramite`, // plain‑text body
