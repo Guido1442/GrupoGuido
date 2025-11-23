f9edc0c8962ff46d34f3b423a1e6f92da6711160
Guido Cardozo <guidocardozo2004@gmail.com>
2025-11-23 12:44:52 -0300

    Mail con esRender

--- src/mail.ts
+++ src/mail.ts
@@ -1,25 +1,49 @@
 import nodemailer from 'nodemailer';
 // codigo base: https://nodemailer.com/
-const transporter = nodemailer.createTransport({
+interface fromPorEstado{
+  estado:string,
+  from:string
+}
+
+const fromPorEstado:fromPorEstado[] = [
+  {estado: '0', from: '"AIDA API" <aidadb2025@gmail.com>'},
+  {estado: '1', from: '"AIDA API" <noreply@aida.com>'}
+]
+
+let transporter:any;
+if(process.env.esRender == '0'){
+ transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.TOKEN_MAIL,
+    },
+  });
+
+}
+else{
+  transporter = nodemailer.createTransport({
   host: "sandbox.smtp.mailtrap.io",
   port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.TOKEN_MAIL,
-  },
-});
-
+    },
+  });
+}
 
 export async function enviarMail(to: string, certificado: string, titulo: string){
-  const mailOptions = {
-        from: '"AIDA API" <noreply@aida.com>',
-        to: to,
-        subject: "Certificado de Titulo en Tramite",
-        text: `Felicidades!! Conseguiste el titulo de ${titulo}. Te enviamos tu certificado de titulo en Tramite`, // plain‑text body
-        html: certificado,
-    };
+    const mailOptions = {
+          from: fromPorEstado.find(e => e.estado === process.env.esRender)?.from,
+          to: to,
+          subject: "Certificado de Titulo en Tramite",
+          text: `Felicidades!! Conseguiste el titulo de ${titulo}. Te enviamos tu certificado de titulo en Tramite`, // plain‑text body
+          html: certificado,
+      };
 
-    return transporter.sendMail(mailOptions);
+      return transporter.sendMail(mailOptions);
+  }
 
-}
+
+
