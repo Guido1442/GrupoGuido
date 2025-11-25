4743443de1ba83a7c60365228e44faf01ee3878a
Guido Cardozo <guidocardozo2004@gmail.com>
2025-11-25 11:04:12 -0300

    esRender

--- recursos/ejemplo-local-sets.bat
+++ recursos/ejemplo-local-sets.bat
@@ -3,7 +3,10 @@ set PGPASSWORD=cambiar_esta_clave
 set PGHOST=localhost
 set PGPORT=5432
 set PGDATABASE=aida_db
+
 SET PORT=3000
 
 SET EMAIL_USER=aidadb2025@gmail.com
 SET TOKEN_MAIL=basa rstk higr xxxx
+
+SET esRender=0
